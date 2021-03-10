bb18b82b77b3679e2e467712d08006ba3db5bd45
Piotr Deren <piotr.deren@op.pl>
2021-03-10 11:03:42 +0100

    Add project files.

---
+++ auCDtect-Fronted/MainForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿
+namespace auCDtect_Fronted
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lstbxFiles = new System.Windows.Forms.ListBox();
+            this.btnAddFiles = new System.Windows.Forms.Button();
+            this.btnRemoveFile = new System.Windows.Forms.Button();
+            this.btnClearList = new System.Windows.Forms.Button();
+            this.btnAnalizeFiles = new System.Windows.Forms.Button();
+            this.btnHelp = new System.Windows.Forms.Button();
+            this.btnAbout = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.rtxtbxOutput = new System.Windows.Forms.RichTextBox();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // lstbxFiles
+            // 
+            this.lstbxFiles.FormattingEnabled = true;
+            this.lstbxFiles.HorizontalScrollbar = true;
+            this.lstbxFiles.Location = new System.Drawing.Point(9, 9);
+            this.lstbxFiles.Name = "lstbxFiles";
+            this.lstbxFiles.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.lstbxFiles.Size = new System.Drawing.Size(384, 238);
+            this.lstbxFiles.TabIndex = 0;
+            this.toolTip.SetToolTip(this.lstbxFiles, "Files for analysis");
+            // 
+            // btnAddFiles
+            // 
+            this.btnAddFiles.Location = new System.Drawing.Point(399, 9);
+            this.btnAddFiles.Name = "btnAddFiles";
+            this.btnAddFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnAddFiles.TabIndex = 1;
+            this.btnAddFiles.Text = "Add files";
+            this.toolTip.SetToolTip(this.btnAddFiles, "Add files to be checked");
+            this.btnAddFiles.UseVisualStyleBackColor = true;
+            this.btnAddFiles.Click += new System.EventHandler(this.btnAddFilesClick);
+            // 
+            // btnRemoveFile
+            // 
+            this.btnRemoveFile.Location = new System.Drawing.Point(399, 38);
+            this.btnRemoveFile.Name = "btnRemoveFile";
+            this.btnRemoveFile.Size = new System.Drawing.Size(75, 23);
+            this.btnRemoveFile.TabIndex = 2;
+            this.btnRemoveFile.Text = "Remove file";
+            this.toolTip.SetToolTip(this.btnRemoveFile, "Remove selected files from the list");
+            this.btnRemoveFile.UseVisualStyleBackColor = true;
+            this.btnRemoveFile.Click += new System.EventHandler(this.btnRemoveFileClick);
+            // 
+            // btnClearList
+            // 
+            this.btnClearList.Location = new System.Drawing.Point(399, 67);
+            this.btnClearList.Name = "btnClearList";
+            this.btnClearList.Size = new System.Drawing.Size(75, 23);
+            this.btnClearList.TabIndex = 3;
+            this.btnClearList.Text = "Clear filelist";
+            this.toolTip.SetToolTip(this.btnClearList, "Clear the list of files to be checked");
+            this.btnClearList.UseVisualStyleBackColor = true;
+            this.btnClearList.Click += new System.EventHandler(this.btnClearListClick);
+            // 
+            // btnAnalizeFiles
+            // 
+            this.btnAnalizeFiles.Location = new System.Drawing.Point(399, 115);
+            this.btnAnalizeFiles.Name = "btnAnalizeFiles";
+            this.btnAnalizeFiles.Size = new System.Drawing.Size(75, 23);
+            this.btnAnalizeFiles.TabIndex = 4;
+            this.btnAnalizeFiles.Text = "Analize files";
+            this.toolTip.SetToolTip(this.btnAnalizeFiles, "Starts the process of analyzing files");
+            this.btnAnalizeFiles.UseVisualStyleBackColor = true;
+            this.btnAnalizeFiles.Click += new System.EventHandler(this.btnAnalizeFilesClick);
+            // 
+            // btnHelp
+            // 
+            this.btnHelp.Location = new System.Drawing.Point(399, 166);
+            this.btnHelp.Name = "btnHelp";
+            this.btnHelp.Size = new System.Drawing.Size(75, 23);
+            this.btnHelp.TabIndex = 5;
+            this.btnHelp.Text = "Help";
+            this.toolTip.SetToolTip(this.btnHelp, "Display help information");
+            this.btnHelp.UseVisualStyleBackColor = true;
+            this.btnHelp.Click += new System.EventHandler(this.btnHelpClick);
+            // 
+            // btnAbout
+            // 
+            this.btnAbout.Location = new System.Drawing.Point(399, 195);
+            this.btnAbout.Name = "btnAbout";
+            this.btnAbout.Size = new System.Drawing.Size(75, 23);
+            this.btnAbout.TabIndex = 6;
+            this.btnAbout.Text = "About";
+            this.toolTip.SetToolTip(this.btnAbout, "Display information about program");
+            this.btnAbout.UseVisualStyleBackColor = true;
+            this.btnAbout.Click += new System.EventHandler(this.btnAboutClick);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(399, 224);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 7;
+            this.btnExit.Text = "Exit";
+            this.toolTip.SetToolTip(this.btnExit, "Exit auCDtect Frontend");
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExitClick);
+            // 
+            // rtxtbxOutput
+            // 
+            this.rtxtbxOutput.CausesValidation = false;
+            this.rtxtbxOutput.Location = new System.Drawing.Point(9, 255);
+            this.rtxtbxOutput.Name = "rtxtbxOutput";
+            this.rtxtbxOutput.ReadOnly = true;
+            this.rtxtbxOutput.Size = new System.Drawing.Size(465, 153);
+            this.rtxtbxOutput.TabIndex = 8;
+            this.rtxtbxOutput.Text = "";
+            this.toolTip.SetToolTip(this.rtxtbxOutput, "The results of the analysis");
+            this.rtxtbxOutput.WordWrap = false;
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.DefaultExt = "wav";
+            this.openFileDialog.Filter = "Music files (*.wav)|*.wav";
+            this.openFileDialog.Multiselect = true;
+            this.openFileDialog.ReadOnlyChecked = true;
+            this.openFileDialog.RestoreDirectory = true;
+            this.openFileDialog.ShowReadOnly = true;
+            // 
+            // MainForm
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 418);
+            this.Controls.Add(this.lstbxFiles);
+            this.Controls.Add(this.btnAddFiles);
+            this.Controls.Add(this.btnRemoveFile);
+            this.Controls.Add(this.btnClearList);
+            this.Controls.Add(this.btnAnalizeFiles);
+            this.Controls.Add(this.btnHelp);
+            this.Controls.Add(this.btnAbout);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.rtxtbxOutput);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "auCDtect Fronted";
+            this.Load += new System.EventHandler(this.MainFormLoad);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.lstbxFilesDragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.lstbxFilesDragEnter);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lstbxFiles;		
+        private System.Windows.Forms.Button btnAddFiles;
+		private System.Windows.Forms.Button btnRemoveFile;
+		private System.Windows.Forms.Button btnClearList;
+		private System.Windows.Forms.Button btnAnalizeFiles;
+		private System.Windows.Forms.Button btnHelp;
+		private System.Windows.Forms.Button btnAbout;
+		private System.Windows.Forms.Button btnExit;
+		private System.Windows.Forms.RichTextBox rtxtbxOutput;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ auCDtect-Fronted/MainForm.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace auCDtect_Fronted
+{
+    public partial class MainForm : Form 
+    {
+        private const string programName = @"tools/auCDtect.exe";
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainFormLoad(object sender, EventArgs e)
+        {
+            if (!(File.Exists(programName)))
+            {
+                MessageBox.Show("auCDtect.exe is not found, auCDtect-frontend can't be used without it. Please place auCDtect.exe in tools directory", "auCDtect.exe not found", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                Close();
+            }
+        }
+
+        private void btnAddFilesClick(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                foreach (String file in openFileDialog.FileNames)
+                {
+                    AddFileToList(file);
+                }
+            }
+        }
+
+        private void btnExitClick(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+        private void lstbxFilesDragDrop(object sender, DragEventArgs e)
+        {
+            string[] fileDropList = (string[]) e.Data.GetData(DataFormats.FileDrop);
+          
+            foreach (string fileDropItem in fileDropList)
+            {
+                // Check whether dropped item is directory
+                if (Directory.Exists(fileDropItem))
+                {
+                     //find all files in that directory
+                     foreach(string fileDropDirItem in Directory.GetFiles(fileDropItem, "*.*", SearchOption.AllDirectories))
+                     {
+                        AddFileToList(fileDropDirItem);   
+                     }
+                }
+                else 
+                {
+                    AddFileToList(fileDropItem);                   
+                }
+            }
+        }
+		
+		private void lstbxFilesDragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                e.Effect = DragDropEffects.Copy;
+            }
+            else
+            {
+                e.Effect = DragDropEffects.None;
+            }
+        }
+		
+        private void AddFileToList(string fullFileName)
+        {
+            string extension = Path.GetExtension(fullFileName);
+            if (extension == ".wav")
+            {               
+                lstbxFiles.Items.Add(fullFileName);
+            }
+        }
+
+        private void btnAboutClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("auCDtect Frontend v1.0, using auCDtect 0.8.2", "auCDtect Frontend version info", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnHelpClick(object sender, EventArgs e)
+        {
+            MessageBox.Show("Place your mouse pointer over an option to get more information", "auCDtect Fronted Help", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void btnRemoveFileClick(object sender, EventArgs e)
+        {
+            /*
+            for (int i = lstbxFiles.SelectedItems.Count - 1; i > -1; i--)
+            {
+                lstbxFiles.Items.Remove(lstbxFiles.SelectedItems[i]);
+            }
+            */
+            while (lstbxFiles.SelectedItems.Count > 0)
+            {
+                lstbxFiles.Items.Remove(lstbxFiles.SelectedItems[0]);
+            }
+        }
+
+        private void btnClearListClick(object sender, EventArgs e)
+        {
+            lstbxFiles.Items.Clear();
+        }
+
+        private void btnAnalizeFilesClick(object sender, EventArgs e)
+        {
+            string format, percent;
+            
+            foreach(string fullFileName in lstbxFiles.Items)
+            {
+                AnalyzeFile(fullFileName, out format, out percent);
+                ApendTextToOutputControl(fullFileName, format, percent);
+            }
+        }
+
+
+        private void ApendTextToOutputControl(string fullFileName, string format, string percent)
+        {
+            string fileName = Path.GetFileName(fullFileName);
+            Color color;
+            switch (format)
+            {
+                case "CDDA":
+                    color = Color.Green;
+                    break;
+                case "MPEG":
+                    color = Color.Red;
+                    break;
+                case "UNKN":
+                    color = Color.Yellow;
+                    break;
+                default:
+                    color = Color.Black;
+                    break;
+            }
+            AppendColorTextToOutputControl(fileName + " => " + format + " [" + percent + "]" + Environment.NewLine, color);
+        }
+
+        private void AppendColorTextToOutputControl(string text, Color color)
+        {
+            rtxtbxOutput.SelectionStart = rtxtbxOutput.TextLength;
+            rtxtbxOutput.SelectionLength = 0;
+            rtxtbxOutput.SelectionColor = color;
+            rtxtbxOutput.AppendText(text);
+            rtxtbxOutput.SelectionColor = rtxtbxOutput.ForeColor;
+        }
+
+        private static void AnalyzeFile(string fileName, out string format, out string percent)
+        {
+            using (Process process = new Process())
+            {
+                process.StartInfo.FileName = programName;
+                process.StartInfo.Arguments += "-m0 ";
+                process.StartInfo.Arguments += "\"";
+                process.StartInfo.Arguments += fileName;
+                process.StartInfo.Arguments += "\"";
+
+                process.StartInfo.UseShellExecute = false;
+                process.StartInfo.RedirectStandardOutput = true;
+                process.StartInfo.CreateNoWindow = true;
+
+                process.Start();
+                process.WaitForExit();
+               
+                string processOutput = process.StandardOutput.ReadToEnd();
+                ParseOutput(processOutput, out format, out percent);
+                process.Close();
+            }
+        }
+
+        private static void ParseOutput(string text, out string format, out string percent)
+        {
+            int startIndex, length;
+            string keyword1 = "looks like", keyword2 = "with", keyword3 = "probability";
+
+            startIndex = text.IndexOf(keyword1) + keyword1.Length + 1;
+            length = text.IndexOf(keyword2, startIndex) - startIndex - 1;
+
+            format = text.Substring(startIndex, length);
+
+            startIndex = text.IndexOf(keyword3, startIndex + length) + keyword3.Length + 1;
+            length = text.IndexOf("%", startIndex) - startIndex + 1;
+
+            percent = text.Substring(startIndex, length);
+        }
+    }
+}
